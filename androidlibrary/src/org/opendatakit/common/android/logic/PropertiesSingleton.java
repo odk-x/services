49f1cdd00a99cafbd9d490d6e119b6d4bcc45522
mitchellsundt <mitchellsundt@gmail.com>
2015-03-17 10:40:04 -0700

    Move the initialization test logic into the PropertiesSingleton. It is now based upon whether the appName has been initialized since the last time core services was started.

--- androidlibrary/src/org/opendatakit/common/android/logic/PropertiesSingleton.java
+++ androidlibrary/src/org/opendatakit/common/android/logic/PropertiesSingleton.java
@@ -18,72 +18,71 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.TreeMap;
 
 import org.apache.commons.lang3.CharEncoding;
+import org.opendatakit.aggregate.odktables.rest.TableConstants;
 import org.opendatakit.common.android.utilities.ODKFileUtils;
 import org.opendatakit.common.android.utilities.WebLogger;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.util.Log;
 
 public class PropertiesSingleton {
 
   private static final String t = "PropertiesSingleton";
-  
+
   private static final String PROPERTIES_FILENAME = "app.properties";
 
   private static final String COMMON_PREFERENCES_CONTEXT = "org.opendatakit.core";
   private static final String COMMON_SHARED_PREFERENCES = "secure_prefs";
-  
+
   private static boolean isMocked = false;
-  
+
   private final String mAppName;
   private long lastModified = 0L;
-  private final TreeMap<String,String> mGeneralDefaults;
+  private final TreeMap<String, String> mGeneralDefaults;
   private final TreeMap<String, String> mSecureDefaults;
-  
+
   private Properties mProps;
   private Context mBaseContext;
 
   private boolean isSecureProperty(String propertyName) {
     return mSecureDefaults.containsKey(propertyName);
   }
-  
+
   void setCurrentContext(Context context) {
     try {
-      mBaseContext = context.createPackageContext(COMMON_PREFERENCES_CONTEXT, 
-          Context.MODE_PRIVATE);
-      if ( isModified() ) {
+      mBaseContext = context.createPackageContext(COMMON_PREFERENCES_CONTEXT, Context.MODE_PRIVATE);
+      if (isModified()) {
         readProperties();
       }
     } catch (Exception e) {
-      if ( isMocked ) {
+      if (isMocked) {
         mBaseContext = context;
       } else {
         boolean faked = false;
         Context app = context.getApplicationContext();
         Class classObj = app.getClass();
         String appName = classObj.getSimpleName();
-        while ( !appName.equals("CommonApplication") ) {
+        while (!appName.equals("CommonApplication")) {
           classObj = classObj.getSuperclass();
-          if ( classObj == null ) break;
+          if (classObj == null)
+            break;
           appName = classObj.getSimpleName();
         }
-        
-        if ( classObj != null ) {
+
+        if (classObj != null) {
           try {
             Class<?>[] argClassList = new Class[] {};
             Method m = classObj.getDeclaredMethod("isMocked", argClassList);
             Object[] argList = new Object[] {};
             Object o = m.invoke(null, argList);
-            if ( ((Boolean) o).booleanValue() ) {
+            if (((Boolean) o).booleanValue()) {
               mBaseContext = context;
               isMocked = true;
               faked = true;
@@ -91,23 +90,23 @@ public class PropertiesSingleton {
           } catch (Exception e1) {
           }
         }
-        if ( !faked ) {
+        if (!faked) {
           e.printStackTrace();
           throw new IllegalStateException("ODK Core Services must be installed!");
         }
       }
     }
   }
-  
-  private SharedPreferences getSharedPreferences() {
-    return mBaseContext.getSharedPreferences(COMMON_SHARED_PREFERENCES, 
-        Context.MODE_PRIVATE | Context.MODE_MULTI_PROCESS);
+
+  private static SharedPreferences getSharedPreferences(Context context) {
+    return context.getSharedPreferences(COMMON_SHARED_PREFERENCES, Context.MODE_PRIVATE
+        | Context.MODE_MULTI_PROCESS);
   }
 
   public boolean containsKey(String propertyName) {
     if (isSecureProperty(propertyName)) {
       // this needs to be stored in a protected area
-      SharedPreferences sharedPreferences = getSharedPreferences();
+      SharedPreferences sharedPreferences = getSharedPreferences(mBaseContext);
       return sharedPreferences.contains(mAppName + "_" + propertyName);
     } else {
       return mProps.containsKey(propertyName);
@@ -115,17 +114,16 @@ public class PropertiesSingleton {
   }
 
   /**
-   * Accesses the given propertyName. This may be stored in
-   * SharedPreferences or in the PROPERTIES_FILENAME in the
-   * assets directory.
-   *  
+   * Accesses the given propertyName. This may be stored in SharedPreferences or
+   * in the PROPERTIES_FILENAME in the assets directory.
+   * 
    * @param propertyName
    * @return null or the string value
    */
   public String getProperty(String propertyName) {
     if (isSecureProperty(propertyName)) {
       // this needs to be stored in a protected area
-      SharedPreferences sharedPreferences = getSharedPreferences();
+      SharedPreferences sharedPreferences = getSharedPreferences(mBaseContext);
       return sharedPreferences.getString(mAppName + "_" + propertyName, null);
     } else {
       return mProps.getProperty(propertyName);
@@ -133,13 +131,12 @@ public class PropertiesSingleton {
   }
 
   /**
-   * Accesses the given propertyName. This may be stored in
-   * SharedPreferences or in the PROPERTIES_FILENAME in the
-   * assets directory.
-   *  
-   * If the value is not specified, null or an empty string,
-   * a null value is returned. Boolean.TRUE is returned
-   * if the value is "true", otherwise Boolean.FALSE is returned.
+   * Accesses the given propertyName. This may be stored in SharedPreferences or
+   * in the PROPERTIES_FILENAME in the assets directory.
+   * 
+   * If the value is not specified, null or an empty string, a null value is
+   * returned. Boolean.TRUE is returned if the value is "true", otherwise
+   * Boolean.FALSE is returned.
    * 
    * @param propertyName
    * @return null or boolean true/false
@@ -147,60 +144,59 @@ public class PropertiesSingleton {
   public Boolean getBooleanProperty(String propertyName) {
     Boolean booleanSetting = Boolean.TRUE;
     String value = getProperty(propertyName);
-    if ( value == null || value.length() == 0 ) {
+    if (value == null || value.length() == 0) {
       return null;
     }
 
-    if ( !"true".equalsIgnoreCase(value)) {
+    if (!"true".equalsIgnoreCase(value)) {
       booleanSetting = Boolean.FALSE;
     }
-    
+
     return booleanSetting;
   }
-  
+
   public void setBooleanProperty(String propertyName, boolean value) {
     setProperty(propertyName, Boolean.toString(value));
   }
 
   /**
-   * Accesses the given propertyName. This may be stored in
-   * SharedPreferences or in the PROPERTIES_FILENAME in the
-   * assets directory.
-   *  
-   * If the value is not specified, null or an empty string,
-   * or if the value cannot be parsed as an integer, then 
-   * null is return. Otherwise, the integer value is returned.
+   * Accesses the given propertyName. This may be stored in SharedPreferences or
+   * in the PROPERTIES_FILENAME in the assets directory.
+   * 
+   * If the value is not specified, null or an empty string, or if the value
+   * cannot be parsed as an integer, then null is return. Otherwise, the integer
+   * value is returned.
    * 
    * @param propertyName
    * @return
    */
   public Integer getIntegerProperty(String propertyName) {
     String value = getProperty(propertyName);
-    if ( value == null ) {
+    if (value == null) {
       return null;
     }
     try {
       int v = Integer.parseInt(value);
       return v;
-    } catch ( NumberFormatException e ) {
+    } catch (NumberFormatException e) {
       return null;
     }
   }
-  
+
   public void setIntegerProperty(String propertyName, int value) {
     setProperty(propertyName, Integer.toString(value));
   }
-  
+
   /**
-   * Caller is responsible for calling writeProperties()
-   * to persist this value to disk.
+   * Caller is responsible for calling writeProperties() to persist this value
+   * to disk.
    * 
    * @param propertyName
    */
   public void removeProperty(String propertyName) {
     if (isSecureProperty(propertyName)) {
       // this needs to be stored in a protected area
-      SharedPreferences sharedPreferences = getSharedPreferences();
+      SharedPreferences sharedPreferences = getSharedPreferences(mBaseContext);
       sharedPreferences.edit().remove(mAppName + "_" + propertyName).commit();
     } else {
       mProps.remove(propertyName);
@@ -208,8 +204,8 @@ public class PropertiesSingleton {
   }
 
   /**
-   * Caller is responsible for calling writeProperties()
-   * to persist this value to disk.
+   * Caller is responsible for calling writeProperties() to persist this value
+   * to disk.
    * 
    * @param propertyName
    * @param value
@@ -217,19 +213,92 @@ public class PropertiesSingleton {
   public void setProperty(String propertyName, String value) {
     if (isSecureProperty(propertyName)) {
       // this needs to be stored in a protected area
-      SharedPreferences sharedPreferences = getSharedPreferences();
+      SharedPreferences sharedPreferences = getSharedPreferences(mBaseContext);
       sharedPreferences.edit().putString(mAppName + "_" + propertyName, value).commit();
     } else {
-      if ( isModified() ) {
-        readProperties(); 
+      if (isModified()) {
+        readProperties();
       }
       mProps.setProperty(propertyName, value);
       writeProperties();
     }
   }
 
-  PropertiesSingleton(Context context, String appName,
-      TreeMap<String,String> plainDefaults, TreeMap<String,String> secureDefaults) {
+  private static String coreStartPropertyName() {
+    return "/core_startTime";
+  }
+
+  /**
+   * Indicate that the core services APK has started
+   *
+   * @param toolName
+   */
+  public static void setStartCoreServices(Context context) {
+    // this needs to be stored in a protected area
+    SharedPreferences sharedPreferences = getSharedPreferences(context);
+    sharedPreferences
+        .edit()
+        .putString(coreStartPropertyName(),
+            TableConstants.nanoSecondsFromMillis(System.currentTimeMillis())).commit();
+  }
+
+  private String toolInitializationPropertyName(String toolName) {
+    return mAppName + "/" + toolName + "_shouldInitialize";
+  }
+
+  /**
+   * Determine whether or not the initialization task for the given toolName
+   * should be run.
+   *
+   * @param toolName
+   *          (e.g., survey, tables, scan, etc.)
+   */
+  public boolean shouldRunInitializationTask(String toolName) {
+    Boolean booleanSetting = Boolean.TRUE;
+    // this needs to be stored in a protected area
+    SharedPreferences sharedPreferences = getSharedPreferences(mBaseContext);
+
+    String value = sharedPreferences.getString(toolInitializationPropertyName(toolName), null);
+    if (value == null || value.length() == 0) {
+      return booleanSetting;
+    }
+
+    String coreValue = sharedPreferences.getString(coreStartPropertyName(), null);
+    if (coreValue == null || coreValue.length() == 0) {
+      return booleanSetting;
+    }
+
+    return value.compareTo(coreValue) <= 0;
+  }
+
+  /**
+   * Indicate that the initialization task for this given tool has been run.
+   *
+   * @param toolName
+   */
+  public void clearRunInitializationTask(String toolName) {
+    // this needs to be stored in a protected area
+    SharedPreferences sharedPreferences = getSharedPreferences(mBaseContext);
+    sharedPreferences
+        .edit()
+        .putString(toolInitializationPropertyName(toolName),
+            TableConstants.nanoSecondsFromMillis(System.currentTimeMillis())).commit();
+  }
+
+  /**
+   * Indicate that the initialization task for this given tool should be run
+   * (again).
+   *
+   * @param toolName
+   */
+  public void setRunInitializationTask(String toolName) {
+    // this needs to be stored in a protected area
+    SharedPreferences sharedPreferences = getSharedPreferences(mBaseContext);
+    sharedPreferences.edit().remove(toolInitializationPropertyName(toolName)).commit();
+  }
+
+  PropertiesSingleton(Context context, String appName, TreeMap<String, String> plainDefaults,
+      TreeMap<String, String> secureDefaults) {
     mAppName = appName;
     mGeneralDefaults = plainDefaults;
     mSecureDefaults = secureDefaults;
@@ -237,7 +306,7 @@ public class PropertiesSingleton {
 
     // Set default values as necessary
     Properties defaults = new Properties();
-    for ( TreeMap.Entry<String, String> entry : mGeneralDefaults.entrySet()) {
+    for (TreeMap.Entry<String, String> entry : mGeneralDefaults.entrySet()) {
       defaults.setProperty(entry.getKey(), entry.getValue());
     }
 
@@ -253,12 +322,13 @@ public class PropertiesSingleton {
     }
 
     // strip out the admin password and store it in the app layer.
-    for ( TreeMap.Entry<String, String> entry : mSecureDefaults.entrySet()) {
-      if ( mProps.containsKey(entry.getKey()) ) {
-        // NOTE: can't use the static methods because this object is not yet fully created
-        SharedPreferences sharedPreferences = getSharedPreferences();
-        sharedPreferences.edit()
-            .putString(mAppName + "_" + entry.getKey(), entry.getValue()).commit();
+    for (TreeMap.Entry<String, String> entry : mSecureDefaults.entrySet()) {
+      if (mProps.containsKey(entry.getKey())) {
+        // NOTE: can't use the static methods because this object is not
+        // yet fully created
+        SharedPreferences sharedPreferences = getSharedPreferences(mBaseContext);
+        sharedPreferences.edit().putString(mAppName + "_" + entry.getKey(), entry.getValue())
+            .commit();
         mProps.remove(entry.getKey());
         dirtyProps = true;
       }
@@ -288,30 +358,30 @@ public class PropertiesSingleton {
   public boolean isModified() {
     File configFile = new File(ODKFileUtils.getAssetsFolder(mAppName), PROPERTIES_FILENAME);
 
-    if ( configFile.exists() ) {
+    if (configFile.exists()) {
       return (lastModified != configFile.lastModified());
     } else {
       return false;
     }
   }
-  
+
   public void readProperties() {
     verifyDirectories();
-    
+
     FileInputStream configFileInputStream = null;
     try {
       File configFile = new File(ODKFileUtils.getAssetsFolder(mAppName), PROPERTIES_FILENAME);
 
-      if ( configFile.exists() ) {
+      if (configFile.exists()) {
         configFileInputStream = new FileInputStream(configFile);
-          
+
         mProps.loadFromXML(configFileInputStream);
         lastModified = configFile.lastModified();
       }
     } catch (Exception e) {
       WebLogger.getLogger(mAppName).printStackTrace(e);
     } finally {
-      if ( configFileInputStream != null ) {
+      if (configFileInputStream != null) {
         try {
           configFileInputStream.close();
         } catch (IOException e) {
@@ -324,9 +394,10 @@ public class PropertiesSingleton {
 
   public void writeProperties() {
     verifyDirectories();
-    
+
     try {
-      File tempConfigFile = new File(ODKFileUtils.getAssetsFolder(mAppName), PROPERTIES_FILENAME + ".temp");
+      File tempConfigFile = new File(ODKFileUtils.getAssetsFolder(mAppName), PROPERTIES_FILENAME
+          + ".temp");
       FileOutputStream configFileOutputStream = new FileOutputStream(tempConfigFile, false);
 
       mProps.storeToXML(configFileOutputStream, null, CharEncoding.UTF_8);
