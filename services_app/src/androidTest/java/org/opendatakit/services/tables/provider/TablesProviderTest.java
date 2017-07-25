682ca87137e329faf894609fa27d007b72641120
Niles Rogoff <me@niles.xyz>
2017-07-24 20:01:50 -0700

    Revert "Fix failing test org.opendatakit.services.database.service.OdkDatabaseServiceImplTest.testGetDefaultGroup"
    This reverts commit 6529f4b0ef623d6b2c6e620a48727d8cf931a93e.

--- services_app/src/androidTest/java/org/opendatakit/services/tables/provider/TablesProviderTest.java
+++ services_app/src/androidTest/java/org/opendatakit/services/tables/provider/TablesProviderTest.java
@@ -47,7 +47,7 @@ public class TablesProviderTest {
     return new Uri.Builder().appendPath(getAppName()).appendPath(id).build();
   }
 
-  public static String get(Cursor c, String col) {
+  private static String get(Cursor c, String col) {
     return c.getString(c.getColumnIndexOrThrow(col));
   }
 
