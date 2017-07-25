3c459b99393e0371e2c4c88f1c9d5c2a44f97684
Niles Rogoff <me@niles.xyz>
2017-07-24 19:59:16 -0700

    Revert "Add admin config setting to allow unsafe authentication over http."
    This reverts commit 778302ed16de0448ee6b440c3e7f047fddee3335.

--- services_app/src/main/java/org/opendatakit/services/sync/service/logic/HttpRestProtocolWrapper.java
+++ services_app/src/main/java/org/opendatakit/services/sync/service/logic/HttpRestProtocolWrapper.java
@@ -282,11 +282,6 @@ public class HttpRestProtocolWrapper {
         asList.add(a);
         a = new AuthScope(host, 8443, null, AuthSchemes.BASIC);
         asList.add(a);
-        // this might be disabled in production builds...
-        if ( sc.getAllowUnsafeAuthentication() ) {
-          a = new AuthScope(host, -1, null, AuthSchemes.BASIC);
-          asList.add(a);
-        }
       }
 
       // add username
