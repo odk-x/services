e2e4a36583e60e85d51a449738206d8ac7ca3a82
Niles Rogoff <me@niles.xyz>
2017-07-24 20:07:12 -0700

    Revert "Lint a few more classes"
    This reverts commit 37824589b8e779ad14c30f75c409527acd513cd4.

--- services_app/src/main/java/org/opendatakit/services/preferences/activities/AdminPasswordChallengeActivity.java
+++ services_app/src/main/java/org/opendatakit/services/preferences/activities/AdminPasswordChallengeActivity.java
@@ -18,88 +18,91 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
 import org.opendatakit.consts.IntentConsts;
 import org.opendatakit.properties.CommonToolProperties;
 import org.opendatakit.properties.PropertiesSingleton;
-import org.opendatakit.services.R;
 import org.opendatakit.utilities.ODKFileUtils;
+import org.opendatakit.services.R;
 
 /**
  * Challenge user with admin password; if successful, display admin-enabled
  * app properties.
  *
  * @author mitchellsundt@gmail.com
+ *
  */
 public class AdminPasswordChallengeActivity extends Activity {
 
-  private static final int APP_PROPERTIES_RESULT_CODE = 1;
+   private static final int APP_PROPERTIES_RESULT_CODE = 1;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+   @Override protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
 
-    final String mAppName;
-    String appName = this.getIntent().getStringExtra(IntentConsts.INTENT_KEY_APP_NAME);
-    if (appName == null || appName.isEmpty()) {
-      mAppName = ODKFileUtils.getOdkDefaultAppName();
-    } else {
-      mAppName = appName;
-    }
-
-    PropertiesSingleton mProps = CommonToolProperties.get(this, mAppName);
-
-    final String adminPwd = mProps.getProperty(CommonToolProperties.KEY_ADMIN_PW);
-    boolean mAdminConfigured = adminPwd != null && !adminPwd.isEmpty();
-
-    if (!mAdminConfigured) {
-      Intent intent = new Intent(this, AppPropertiesActivity.class);
-      intent.putExtra(IntentConsts.INTENT_KEY_APP_NAME, mAppName);
-      startActivityForResult(intent, APP_PROPERTIES_RESULT_CODE);
-      return;
-    }
-
-    setContentView(R.layout.password_challenge_dialog_layout);
-
-    View positiveButton = findViewById(R.id.positive_button);
-    positiveButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        TextView passwordEditText = (TextView) findViewById(R.id.pwd_field);
-        String pw = passwordEditText.getText().toString();
-
-        if (adminPwd.equals(pw)) {
-          Intent intent = new Intent(AdminPasswordChallengeActivity.this,
-              AppPropertiesActivity.class);
-          intent.putExtra(IntentConsts.INTENT_KEY_APP_NAME, mAppName);
-          intent.putExtra(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE, true);
-          startActivityForResult(intent, APP_PROPERTIES_RESULT_CODE);
-        } else {
-          Toast.makeText(AdminPasswordChallengeActivity.this, R.string.password_mismatch,
-              Toast.LENGTH_SHORT).show();
-        }
-      }
-    });
-
-    View negativeButton = findViewById(R.id.negative_button);
-    negativeButton.setOnClickListener(new View.OnClickListener() {
-
-      @Override
-      public void onClick(View v) {
-        setResult(RESULT_OK);
-        finish();
+      final String mAppName;
+      String appName = this.getIntent().getStringExtra(IntentConsts.INTENT_KEY_APP_NAME);
+      if (appName == null || appName.length() == 0) {
+         mAppName = ODKFileUtils.getOdkDefaultAppName();
+      } else {
+         mAppName = appName;
       }
 
-    });
-  }
+      PropertiesSingleton mProps = CommonToolProperties.get(this, mAppName);
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode == APP_PROPERTIES_RESULT_CODE) {
-      setResult(resultCode);
-      finish();
-    }
-  }
+      final String adminPwd = mProps.getProperty(CommonToolProperties.KEY_ADMIN_PW);
+      boolean mAdminConfigured = (adminPwd != null && adminPwd.length() != 0);
+
+      if ( !mAdminConfigured ) {
+         Intent intent = new Intent(this, AppPropertiesActivity.class );
+         intent.putExtra(IntentConsts.INTENT_KEY_APP_NAME, mAppName);
+         startActivityForResult(intent, APP_PROPERTIES_RESULT_CODE);
+         return;
+      }
+
+      setContentView(R.layout.password_challenge_dialog_layout);
+
+      Button positiveButton = (Button) findViewById(R.id.positive_button);
+      positiveButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            EditText passwordEditText = (EditText) findViewById(R.id.pwd_field);
+            String pw = passwordEditText.getText().toString();
+
+            if (adminPwd.equals(pw)) {
+               Intent intent = new Intent(AdminPasswordChallengeActivity.this,
+                   AppPropertiesActivity.class );
+               intent.putExtra(IntentConsts.INTENT_KEY_APP_NAME, mAppName);
+               intent.putExtra(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE, true);
+               startActivityForResult(intent, APP_PROPERTIES_RESULT_CODE);
+               return;
+            } else {
+               Toast.makeText(AdminPasswordChallengeActivity.this,
+                   R.string.password_mismatch, Toast.LENGTH_SHORT).show();
+            }
+         }
+      });
+
+      Button negativeButton = (Button) findViewById(R.id.negative_button);
+      negativeButton.setOnClickListener(new View.OnClickListener() {
+
+         @Override
+         public void onClick(View v) {
+            setResult(RESULT_OK);
+            finish();
+         }
+
+      });
+   }
+
+   @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+      super.onActivityResult(requestCode, resultCode, data);
+      if ( requestCode == APP_PROPERTIES_RESULT_CODE ) {
+         setResult(resultCode);
+         finish();
+         return;
+      }
+   }
 }
--- services_app/src/main/java/org/opendatakit/services/preferences/activities/AppPropertiesActivity.java
+++ services_app/src/main/java/org/opendatakit/services/preferences/activities/AppPropertiesActivity.java
@@ -21,12 +21,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.support.annotation.StringRes;
+
 import org.opendatakit.consts.IntentConsts;
 import org.opendatakit.properties.CommonToolProperties;
 import org.opendatakit.properties.PropertiesSingleton;
+import org.opendatakit.services.sync.actions.fragments.VerifyServerSettingsFragment;
+import org.opendatakit.utilities.ODKFileUtils;
 import org.opendatakit.services.R;
 import org.opendatakit.services.sync.actions.activities.VerifyServerSettingsActivity;
-import org.opendatakit.utilities.ODKFileUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,21 +40,16 @@ import java.util.List;
  */
 public class AppPropertiesActivity extends PreferenceActivity implements IOdkAppPropertiesActivity {
 
-  /**
-   * Activity code for startActivityForResult
-   */
+  private static final String t = "AppPropertiesActivity";
+
   public static final int SPLASH_IMAGE_CHOOSER = 1;
-  /**
-   * Used for logging
-   */
-  @SuppressWarnings("unused")
-  private static final String TAG = AppPropertiesActivity.class.getSimpleName();
+
   private static final String SAVED_ADMIN_CONFIGURED = "savedAdminConfigured";
   private String mAppName;
   private boolean mAdminMode;
   private boolean mAdminConfigured;
   private Activity mActivity = this;
-
+  
   private PropertiesSingleton mProps;
 
   /**
@@ -60,45 +57,45 @@ public class AppPropertiesActivity extends PreferenceActivity implements IOdkApp
    */
   @Override
   public void onBuildHeaders(List<Header> target) {
-    List<Header> rawHeaders = new ArrayList<>();
+    List<Header> rawHeaders = new ArrayList<Header>();
     loadHeadersFromResource(R.xml.preferences_headers, rawHeaders);
 
-    for (Header header : rawHeaders) {
+    for ( Header header : rawHeaders ) {
       // if the properties are only shown in adminMode,
       // then omit them if we are not in admin mode.
-      if (header.fragmentArguments != null) {
-        boolean hasAdminModeRestriction = header.fragmentArguments
-            .containsKey(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE);
-        boolean hasAdminEnabledRestriction = header.fragmentArguments
-            .containsKey(IntentConsts.INTENT_KEY_SETTINGS_ADMIN_ENABLED);
+      if ( header.fragmentArguments != null ) {
+        boolean hasAdminModeRestriction =
+          header.fragmentArguments.containsKey(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE);
+        boolean hasAdminEnabledRestriction =
+          header.fragmentArguments.containsKey(IntentConsts.INTENT_KEY_SETTINGS_ADMIN_ENABLED);
 
         boolean pass = !(hasAdminEnabledRestriction || hasAdminModeRestriction);
-        if (hasAdminEnabledRestriction) {
-          boolean enabledRequirement = header.fragmentArguments
-              .getBoolean(IntentConsts.INTENT_KEY_SETTINGS_ADMIN_ENABLED);
-          pass = enabledRequirement == mAdminConfigured;
+        if ( hasAdminEnabledRestriction ) {
+          boolean enabledRequirement = header.fragmentArguments.getBoolean(IntentConsts
+              .INTENT_KEY_SETTINGS_ADMIN_ENABLED);
+          pass = pass || ( enabledRequirement == mAdminConfigured);
         }
 
-        if (hasAdminModeRestriction) {
-          boolean modeRestriction = header.fragmentArguments
-              .getBoolean(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE);
-          pass = pass || modeRestriction == mAdminMode;
+        if ( hasAdminModeRestriction ) {
+          boolean modeRestriction = header.fragmentArguments.getBoolean(IntentConsts
+              .INTENT_KEY_SETTINGS_IN_ADMIN_MODE);
+          pass = pass || ( modeRestriction == mAdminMode );
         }
 
-        if (!pass) {
+        if ( !pass ) {
           continue;
         }
       }
 
-      if (header.id == R.id.general_settings_in_admin_mode) {
-        if (!mAdminConfigured || mAdminMode) {
+      if ( header.id == R.id.general_settings_in_admin_mode ) {
+        if ( !mAdminConfigured || mAdminMode ) {
           continue;
         }
       }
 
-      if (header.id == R.id.clear_configuration_settingss) {
+      if ( header.id == R.id.clear_configuration_settingss ) {
         // omit this if we have admin mode configured but are not in admin mode
-        if (mAdminConfigured && !mAdminMode) {
+        if ( mAdminConfigured && !mAdminMode ) {
           continue;
         }
       }
@@ -106,8 +103,8 @@ public class AppPropertiesActivity extends PreferenceActivity implements IOdkApp
       target.add(header);
     }
 
-    for (Header header : target) {
-      if (header.id == R.id.general_settings_in_admin_mode) {
+    for ( Header header : target ) {
+      if ( header.id == R.id.general_settings_in_admin_mode ) {
         // TODO: change to challenge for admin password and then
         // TODO: launch the general settings in admin mode.
         Intent intent = new Intent(this, AdminPasswordChallengeActivity.class);
@@ -116,22 +113,22 @@ public class AppPropertiesActivity extends PreferenceActivity implements IOdkApp
         header.intent = intent;
       }
 
-      if (header.id == R.id.clear_configuration_settingss) {
+      if ( header.id == R.id.clear_configuration_settingss ) {
         Intent intent = new Intent(this, ClearAppPropertiesActivity.class);
         intent.putExtra(IntentConsts.INTENT_KEY_APP_NAME,
             this.getIntent().getStringExtra(IntentConsts.INTENT_KEY_APP_NAME));
         header.intent = intent;
       }
 
-      if (header.id == R.id.verify_server_settingss) {
+      if ( header.id == R.id.verify_server_settingss ) {
         Intent intent = new Intent(this, VerifyServerSettingsActivity.class);
         intent.putExtra(IntentConsts.INTENT_KEY_APP_NAME,
-            this.getIntent().getStringExtra(IntentConsts.INTENT_KEY_APP_NAME));
+                this.getIntent().getStringExtra(IntentConsts.INTENT_KEY_APP_NAME));
         header.intent = intent;
       }
 
-      if (header.id == R.id.admin_password_status) {
-        if (mAdminConfigured) {
+      if ( header.id == R.id.admin_password_status ) {
+        if ( mAdminConfigured ) {
           header.titleRes = R.string.change_admin_password;
           header.summaryRes = R.string.admin_password_enabled;
         } else {
@@ -139,11 +136,12 @@ public class AppPropertiesActivity extends PreferenceActivity implements IOdkApp
           header.summaryRes = R.string.admin_password_disabled;
         }
       }
-      if (header.fragmentArguments != null) {
+      if ( header.fragmentArguments != null ) {
         header.fragmentArguments.putString(IntentConsts.INTENT_KEY_APP_NAME,
             this.getIntent().getStringExtra(IntentConsts.INTENT_KEY_APP_NAME));
-        if (mAdminMode) {
-          header.fragmentArguments.putBoolean(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE, true);
+        if ( mAdminMode ) {
+          header.fragmentArguments.putBoolean(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE,
+              true);
         } else {
           header.fragmentArguments.remove(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE);
         }
@@ -164,55 +162,54 @@ public class AppPropertiesActivity extends PreferenceActivity implements IOdkApp
   protected void onCreate(Bundle savedInstanceState) {
 
     mAppName = this.getIntent().getStringExtra(IntentConsts.INTENT_KEY_APP_NAME);
-    if (mAppName == null || mAppName.isEmpty()) {
+    if (mAppName == null || mAppName.length() == 0) {
       mAppName = ODKFileUtils.getOdkDefaultAppName();
     }
 
     mProps = CommonToolProperties.get(this, mAppName);
 
-    if (savedInstanceState != null) {
+    if ( savedInstanceState != null ) {
       mAdminConfigured = savedInstanceState.getBoolean(SAVED_ADMIN_CONFIGURED);
     } else {
       String adminPwd = mProps.getProperty(CommonToolProperties.KEY_ADMIN_PW);
-      mAdminConfigured = adminPwd != null && !adminPwd.isEmpty();
+      mAdminConfigured = (adminPwd != null && adminPwd.length() != 0);
     }
 
-    mAdminMode = this.getIntent()
-        .getBooleanExtra(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE, false);
+    mAdminMode =
+        this.getIntent().getBooleanExtra(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE, false);
 
-    if (getActionBar() != null) {
-      getActionBar().setTitle(getString(mAdminMode ?
-          R.string.action_bar_general_settings_admin_mode :
-          R.string.action_bar_general_settings, mAppName));
-    }
+    this.getActionBar().setTitle(
+        getString((mAdminMode ?
+            R.string.action_bar_general_settings_admin_mode :
+            R.string.action_bar_general_settings),
+            mAppName));
 
     super.onCreate(savedInstanceState);
   }
 
-  @Override
-  protected void onResume() {
+  @Override protected void onResume() {
     super.onResume();
 
     mAppName = this.getIntent().getStringExtra(IntentConsts.INTENT_KEY_APP_NAME);
-    if (mAppName == null || mAppName.isEmpty()) {
+    if (mAppName == null || mAppName.length() == 0) {
       mAppName = ODKFileUtils.getOdkDefaultAppName();
     }
 
     mProps = CommonToolProperties.get(this, mAppName);
 
-    mAdminMode = this.getIntent()
-        .getBooleanExtra(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE, false);
+    mAdminMode =
+        this.getIntent().getBooleanExtra(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE, false);
 
     String adminPwd = mProps.getProperty(CommonToolProperties.KEY_ADMIN_PW);
-    boolean isAdminConfigured = adminPwd != null && !adminPwd.isEmpty();
+    boolean isAdminConfigured = (adminPwd != null && adminPwd.length() != 0);
 
     boolean shouldInvalidateHeaders = false;
-    if (isAdminConfigured != mAdminConfigured) {
+    if ( isAdminConfigured != mAdminConfigured ) {
       mAdminConfigured = isAdminConfigured;
       shouldInvalidateHeaders = true;
     }
 
-    if (mAdminMode && !mAdminConfigured) {
+    if ( mAdminMode && !mAdminConfigured ) {
       // we disabled admin controls but are in the admin-level
       // settings activity.
       // back out to the non-admin-level settings activity.
@@ -220,7 +217,7 @@ public class AppPropertiesActivity extends PreferenceActivity implements IOdkApp
       return;
     }
 
-    if (shouldInvalidateHeaders) {
+    if ( shouldInvalidateHeaders ) {
       invalidateHeaders();
     }
   }
@@ -230,8 +227,7 @@ public class AppPropertiesActivity extends PreferenceActivity implements IOdkApp
       @StringRes int titleRes, int shortTitleRes) {
     Intent toLaunch = super.onBuildStartFragmentIntent(fragmentName, args, titleRes, shortTitleRes);
     toLaunch.putExtra(IntentConsts.INTENT_KEY_APP_NAME, mAppName);
-    if (mAdminMode) {
-      //noinspection ConstantConditions
+    if ( mAdminMode ) {
       toLaunch.putExtra(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE, mAdminMode);
     }
     return toLaunch;
@@ -246,17 +242,17 @@ public class AppPropertiesActivity extends PreferenceActivity implements IOdkApp
   /**
    * If we are exiting the non-privileged settings screen and the user roles are not
    * set, then prompt the user to verify user permissions or lose their changes
-   * <p>
+   *
    * Otherwise, exit the settings screen.
    */
   @Override
   public void onBackPressed() {
-    if (!mAdminMode) {
+    if ( !mAdminMode ) {
       String authType = mProps.getProperty(CommonToolProperties.KEY_AUTHENTICATION_TYPE);
-      boolean isAnonymous =
-          authType == null || authType.isEmpty() || getString(R.string.credential_type_none)
-              .equals(authType);
-      if (mProps.getProperty(CommonToolProperties.KEY_ROLES_LIST).isEmpty() && !isAnonymous) {
+      boolean isAnonymous = (authType == null) || (authType.length() == 0) ||
+              getString(R.string.credential_type_none).equals(authType);
+      if ( mProps.getProperty(CommonToolProperties.KEY_ROLES_LIST).length() == 0 &&
+              !isAnonymous ) {
 
         promptToVerifyCredentials();
         return;
