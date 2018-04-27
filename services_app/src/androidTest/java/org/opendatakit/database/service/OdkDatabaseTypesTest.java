e0beec3e3e49d6c17c8197df6f49605ba077ae21
clarlars <clarlars@gmail.com>
2018-04-26 17:28:41 -0700

    Add test for checkpoint and array type

--- services_app/src/androidTest/java/org/opendatakit/database/service/OdkDatabaseTypesTest.java
+++ services_app/src/androidTest/java/org/opendatakit/database/service/OdkDatabaseTypesTest.java
@@ -14,6 +14,7 @@ import org.opendatakit.exception.ActionNotAuthorizedException;
 import org.opendatakit.exception.ServicesAvailabilityException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,12 +32,18 @@ public class OdkDatabaseTypesTest extends OdkDatabaseTestAbstractBase {
    private static final String COL_BOOL_ID = "columnBool";
    private static final String COL_ROWPATH_ID = "columnRowPath";
    private static final String COL_CONFIGPATH_ID = "columnConfigPath";
-   private static final String COL_ARRAY_ID = "columnArrayPath";
    private static final String COL_STRING_ID = "columnString";
-   private static final String COL_OBJ_ID = "columnObject";
+   private static final String COL_ARRAY_ID = "columnArrayPath";
+   private static final String COL_ITEM_STRING_ID = "columnArrayPath_items";
+   private static final String COL_GEO_OBJ_ID = "columnGeopointObject";
+   private static final String COL_GEO_OBJ_ID_LAT = "columnGeopointObject_lat";
+   private static final String COL_GEO_OBJ_ID_LONG = "columnGeopointObject_long";
+   private static final String COL_GEO_OBJ_ID_ALT = "columnGeopointObject_alt";
+   private static final String COL_GEO_OBJ_ID_ACC = "columnGeopointObject_acc";
 
    private static final String TEST_STR_1 = "TestBoolStr1";
    private static final int TEST_INT_1 = 1;
+   private static final double TEST_NUM_1 = 1.0;
    private static final boolean TEST_BOOL_1 = true;
 
    private static final int TEST_INT_i = 0;
@@ -44,6 +51,12 @@ public class OdkDatabaseTypesTest extends OdkDatabaseTestAbstractBase {
    private static final String TEST_ROWPATH_i = "TestRowPath";
    private static final String TEST_CONFIGPATH_i = "TestConfigPath";
    private static final String TEST_STR_i = "TestBoolStr";
+   private static final String TEST_ARRAY_i = "[\"Test1\",\"Test2\"]";
+
+   private static final ArrayList<String> TEST_ARRAY_i_CHECK = new ArrayList<String>();
+   static {
+      TEST_ARRAY_i_CHECK.addAll(Arrays.asList("Test1", "Test2"));
+   }
 
 
    @Override protected void setUpBefore() {
@@ -65,12 +78,26 @@ public class OdkDatabaseTypesTest extends OdkDatabaseTestAbstractBase {
       columns.add(new Column(COL_ROWPATH_ID, "column Rowpath", ElementDataType.rowpath.name(),
           null));
       columns.add(new Column(COL_CONFIGPATH_ID, "column Configpath", ElementDataType.configpath
-          .name
-          (), null));
- //     columns.add(new Column(COL_ARRAY_ID, "column Array", ElementDataType.array.name(), null));
-      columns.add(new Column(COL_STRING_ID, "column String", ElementDataType.string.name(),
+          .name(), null));
+      columns.add(new Column(COL_STRING_ID, "columnString", ElementDataType.string.name(),
           null));
-  //    columns.add(new Column(COL_OBJ_ID, "column Object", ElementDataType.object.name(),null));
+
+      columns.add(new Column(COL_ITEM_STRING_ID, "items", ElementDataType.string
+          .name(), null));
+      columns.add(new Column(COL_ARRAY_ID, "columnArrayPath", ElementDataType.array.name(),
+          "[\"" + COL_ITEM_STRING_ID + "\"]"));
+
+      columns.add(new Column(COL_GEO_OBJ_ID_ACC, "acc", ElementDataType.number.name(),
+          null));
+      columns.add(new Column(COL_GEO_OBJ_ID_ALT, "alt", ElementDataType.number.name(),
+          null));
+      columns.add(new Column(COL_GEO_OBJ_ID_LAT, "lat", ElementDataType.number.name(),
+          null));
+      columns.add(new Column(COL_GEO_OBJ_ID_LONG, "long", ElementDataType.number.name(),
+          null));
+      columns.add(new Column(COL_GEO_OBJ_ID, COL_GEO_OBJ_ID, ElementDataType.object.name(),
+          "[\"" + COL_GEO_OBJ_ID_ACC + "\",\"" + COL_GEO_OBJ_ID_ALT + "\",\"" + COL_GEO_OBJ_ID_LAT + "\",\"" + COL_GEO_OBJ_ID_LONG
+              + "\"]"));
 
       return columns;
    }
@@ -82,21 +109,29 @@ public class OdkDatabaseTypesTest extends OdkDatabaseTestAbstractBase {
       cv.put(COL_INTEGER_ID, TEST_INT_1);
       cv.put(COL_BOOL_ID, TEST_BOOL_1);
 
+      cv.put(COL_GEO_OBJ_ID_ACC, TEST_NUM_1);
+      cv.put(COL_GEO_OBJ_ID_ALT, TEST_NUM_1);
+      cv.put(COL_GEO_OBJ_ID_LAT, TEST_NUM_1);
+      cv.put(COL_GEO_OBJ_ID_LONG, TEST_NUM_1);
+
       return cv;
    }
 
    private ContentValues contentValuesTestSeti(int i) {
       ContentValues cv = new ContentValues();
 
-
       cv.put(COL_INTEGER_ID, TEST_INT_i + i);
       cv.put(COL_NUMBER_ID, TEST_NUM_i + i);
       cv.put(COL_BOOL_ID, (i%2 != 0));
       cv.put(COL_ROWPATH_ID, TEST_ROWPATH_i + i);
       cv.put(COL_CONFIGPATH_ID, TEST_CONFIGPATH_i + i);
-      //cv.put(COL_ARRAY_ID,????);
+      cv.put(COL_ARRAY_ID, TEST_ARRAY_i);
       cv.put(COL_STRING_ID, TEST_STR_i + i);
-      //cv.put(COL_OBJ_ID,????);
+      cv.put(COL_GEO_OBJ_ID_ACC, TEST_NUM_i + i);
+      cv.put(COL_GEO_OBJ_ID_ALT, TEST_NUM_i + i);
+      cv.put(COL_GEO_OBJ_ID_LAT, TEST_NUM_i + i);
+      cv.put(COL_GEO_OBJ_ID_LONG, TEST_NUM_i + i);
+
       return cv;
    }
 
@@ -106,9 +141,14 @@ public class OdkDatabaseTypesTest extends OdkDatabaseTestAbstractBase {
       assertEquals(row.getDataByKey(COL_BOOL_ID), Boolean.valueOf(TEST_BOOL_1));
       assertNull(row.getDataByKey(COL_ROWPATH_ID));
       assertNull(row.getDataByKey(COL_CONFIGPATH_ID));
- //     assertNull(row.getDataByKey(COL_ARRAY_ID));
+      assertNull(row.getDataByKey(COL_ARRAY_ID));
       assertEquals(row.getDataByKey(COL_STRING_ID), TEST_STR_1);
-  //    assertNull(row.getDataByKey(COL_OBJ_ID));
+
+      assertEquals(row.getDataByKey(COL_GEO_OBJ_ID_ACC), Double.valueOf(TEST_NUM_1));
+      assertEquals(row.getDataByKey(COL_GEO_OBJ_ID_ALT), Double.valueOf(TEST_NUM_1));
+      assertEquals(row.getDataByKey(COL_GEO_OBJ_ID_LAT), Double.valueOf(TEST_NUM_1));
+      assertEquals(row.getDataByKey(COL_GEO_OBJ_ID_LONG), Double.valueOf(TEST_NUM_1));
+
    }
 
    private void verifyRowTestSeti(TypedRow row, int i) {
@@ -117,10 +157,13 @@ public class OdkDatabaseTypesTest extends OdkDatabaseTestAbstractBase {
       assertEquals(row.getDataByKey(COL_BOOL_ID), Boolean.valueOf((i%2 != 0)));
       assertEquals(row.getDataByKey(COL_ROWPATH_ID), TEST_ROWPATH_i + i);
       assertEquals(row.getDataByKey(COL_CONFIGPATH_ID), TEST_CONFIGPATH_i + i);
-     // assertEquals(row.getDataByKey(COL_ARRAY_ID), ???);
       assertEquals(row.getDataByKey(COL_STRING_ID), TEST_STR_i + i);
-     // assertEquals(row.getDataByKey(COL_OBJ_ID), ???);
-   }
+
+      assertEquals(row.getDataByKey(COL_ARRAY_ID), TEST_ARRAY_i_CHECK);
+      assertEquals(row.getDataByKey(COL_GEO_OBJ_ID_ACC), Double.valueOf(TEST_NUM_i + i));
+      assertEquals(row.getDataByKey(COL_GEO_OBJ_ID_ALT), Double.valueOf(TEST_NUM_i + i));
+      assertEquals(row.getDataByKey(COL_GEO_OBJ_ID_LAT), Double.valueOf(TEST_NUM_i + i));
+      assertEquals(row.getDataByKey(COL_GEO_OBJ_ID_LONG), Double.valueOf(TEST_NUM_i + i));   }
 
    @Test
    public void testDbInsertSingleRowIntoTable() throws ActionNotAuthorizedException {
@@ -240,6 +283,46 @@ public class OdkDatabaseTypesTest extends OdkDatabaseTestAbstractBase {
                // let exception proceed
             }
          }
+      }
+   }
+
+   @Test
+   public void testDbInsertCheckpointRowWithBooleanIntoTable() throws ActionNotAuthorizedException {
+      UserDbInterface serviceInterface = bindToDbService();
+      try {
+
+         List<Column> columnList = createColumnList();
+         ColumnList colList = new ColumnList(columnList);
+
+         DbHandle db = serviceInterface.openDatabase(APPNAME);
+         Log.i("openDatabase", "testDbInsertCheckpointRowWithBooleanIntoTable: " + db.getDatabaseHandle());
+         serviceInterface.createOrOpenTableWithColumns(APPNAME, db, DB_TABLE_ID, colList);
+
+         OrderedColumns columns = new OrderedColumns(APPNAME, DB_TABLE_ID, columnList);
+         UUID rowId = UUID.randomUUID();
+
+         serviceInterface.insertCheckpointRowWithId(APPNAME, db, DB_TABLE_ID, columns, contentValuesTestSet1(),
+             rowId.toString());
+
+         UserTable table = serviceInterface
+             .getRowsWithId(APPNAME, db, DB_TABLE_ID, columns, rowId.toString());
+
+         assertEquals(DB_TABLE_ID, table.getTableId());
+         assertEquals(1, table.getNumberOfRows());
+         TypedRow row = table.getRowAtIndex(0);
+
+         verifyRowTestSet1(row);
+
+         // clean up
+         serviceInterface.deleteTableAndAllData(APPNAME, db, DB_TABLE_ID);
+
+         // verify no tables left
+         assertTrue(hasNoTablesInDb(serviceInterface, db));
+         serviceInterface.closeDatabase(APPNAME, db);
+         Log.i("closeDatabase", "testDbInsertSingleRowIntoTable: " + db.getDatabaseHandle());
+      } catch (ServicesAvailabilityException e) {
+         e.printStackTrace();
+         fail(e.getMessage());
       }
    }
 }
