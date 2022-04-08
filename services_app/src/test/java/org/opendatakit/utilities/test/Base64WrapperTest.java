040682c646f6414b22b89bd307f3c958ab9d57a9
niyolynate <98440370+niyolynate@users.noreply.github.com>
2022-04-08 06:23:21 +0300

    added wrapper tests

--- services_app/src/test/java/org/opendatakit/utilities/test/Base64WrapperTest.java
+++ services_app/src/test/java/org/opendatakit/utilities/test/Base64WrapperTest.java
@@ -11,5 +11,13 @@ public class Base64WrapperTest {
     @Test
     public void testWrapperCreation() throws ClassNotFoundException {
         Base64Wrapper wrapper = new Base64Wrapper("unittestTMP");
+
+        public void wrapperTest {
+
+            task wrapper(type:wrapper) {
+                gradleVersion = '4.3.1'
+
+                public ActivityWrapperRule<MainActivity> activityRule =
+                        new ActivityWrapperRule<MainActivity>(MainActivity.class);
     }
 }
