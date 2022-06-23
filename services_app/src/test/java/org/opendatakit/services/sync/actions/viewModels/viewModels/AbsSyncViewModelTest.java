bd2642d711f8cde457b467f6a1c41a938b76cdad
sanyud24 <sanyu.daver.civ19@itbhu.ac.in>
2022-06-23 20:23:19 +0530

    added tests for  AbsSyncViewModel

--- services_app/src/test/java/org/opendatakit/services/sync/actions/viewModels/viewModels/AbsSyncViewModelTest.java
+++ services_app/src/test/java/org/opendatakit/services/sync/actions/viewModels/viewModels/AbsSyncViewModelTest.java
@@ -29,45 +29,45 @@ public class AbsSyncViewModelTest {
     }
 
     @Test
-    public void checkAppName() throws Exception {
+    public void checkIfAppName_isAvailable() throws Exception {
         String appName = "appName";
         absSyncViewModel.setAppName(appName);
         assertEquals(appName, absSyncViewModel.getAppName());
     }
 
     @Test
-    public void checkIfStarted() {
+    public void checkIfAbsSyncViewModel_isStarted() {
         absSyncViewModel.setStarted(true);
         assertTrue(absSyncViewModel.getStarted());
     }
 
     @Test
-    public void checkIfIsFirstLaunched() throws Exception {
+    public void checkIfIsFirstLaunched_isAllowed() throws Exception {
         absSyncViewModel.setIsFirstLaunch(true);
         assertEquals(true, absSyncViewModel.checkIsFirstLaunch().getValue());
     }
 
     @Test
-    public void checkServerUrl() throws Exception {
+    public void checkIfServerUrl_isAvailable() throws Exception {
         String serverUrl = "https://odk-x.org/";
         absSyncViewModel.setServerUrl(serverUrl);
         assertEquals(serverUrl, absSyncViewModel.getServerUrl().getValue());
     }
 
     @Test
-    public void checkIfIsServerVerified() {
+    public void checkIfServer_isVerified() {
         absSyncViewModel.setIsServerVerified(true);
         assertEquals(true, absSyncViewModel.checkIsServerVerified().getValue());
     }
 
     @Test
-    public void checkIfIsAnonymousSignInUsed() {
+    public void checkIfAnonymousSignInUsed_isAllowed() {
         absSyncViewModel.setIsAnonymousSignInUsed(true);
         assertEquals(true, absSyncViewModel.checkIsAnonymousSignInUsed().getValue());
     }
 
     @Test
-    public void checkIsAnonymousAllowed() {
+    public void checkIfAnonymous_isAllowed() {
         absSyncViewModel.setIsAnonymousAllowed(true);
         assertEquals(true, absSyncViewModel.checkIsAnonymousAllowed().getValue());
         assertTrue(absSyncViewModel.isAnonymousAllowed());
@@ -75,40 +75,40 @@ public class AbsSyncViewModelTest {
     }
 
     @Test
-    public void checkCurrentUserState() {
+    public void checkIfCurrentUserState_isAvailable() {
         absSyncViewModel.setCurrentUserState(UserState.LOGGED_OUT);
         assertEquals(UserState.LOGGED_OUT, absSyncViewModel.getCurrentUserState().getValue());
         assertNotNull(absSyncViewModel.getUserState());
     }
 
     @Test
-    public void checkUserName() {
+    public void checkIfUserName_isAvailable() {
         String userName = "john";
         absSyncViewModel.setUsername(userName);
         assertEquals(userName, absSyncViewModel.getUsername().getValue());
     }
 
     @Test
-    public void checkIsUserVerified() {
+    public void checkIfUser_isVerified() {
         absSyncViewModel.setIsUserVerified(true);
         assertEquals(true, absSyncViewModel.checkIsUserVerified().getValue());
     }
 
     @Test
-    public void checkLastSyncTime() {
+    public void checkIfLastSyncTime_isVisible() {
         long time = LocalTime.parse("12:30").getHour();
         absSyncViewModel.setLastSyncTime(time);
         assertEquals(String.valueOf(time), absSyncViewModel.getLastSyncTime().getValue().toString());
     }
 
     @Test
-    public void checkIsLastSyncTimeAvailable() {
+    public void checkIFLastSyncTimeAvailable_isAllowed() {
         absSyncViewModel.setIsLastSyncTimeAvailable(true);
         assertEquals(true, absSyncViewModel.checkIsLastSyncTimeAvailable().getValue());
     }
 
     @Test
-    public void checkSyncAttachmentState() {
+    public void checkIfSyncAttachmentState_isAvailable() {
         absSyncViewModel.updateSyncAttachmentState(SyncAttachmentState.REDUCED_SYNC);
         assertEquals(SyncAttachmentState.REDUCED_SYNC, absSyncViewModel.getCurrentSyncAttachmentState());
         absSyncViewModel.getSyncAttachmentState();
