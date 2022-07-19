c60f5992ac5024cb981605f32622c85bd1c4f8ee
sanyud24 <sanyu.daver.civ19@itbhu.ac.in>
2022-07-20 00:29:46 +0530

    added custom viewaction method

--- services_app/src/androidTest/java/org/opendatakit/BaseUITest.java
+++ services_app/src/androidTest/java/org/opendatakit/BaseUITest.java
@@ -1,13 +1,21 @@
 package org.opendatakit;
 
+import static org.hamcrest.Matchers.isA;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Checkable;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
 import org.junit.After;
 import org.junit.Before;
 
@@ -36,4 +44,43 @@ public abstract class BaseUITest<T extends Activity> {
     protected Context getContext() {
         return InstrumentationRegistry.getInstrumentation().getTargetContext();
     }
+
+    public static ViewAction setChecked(final boolean checked) {
+        return new ViewAction() {
+            @Override
+            public BaseMatcher<View> getConstraints() {
+                return new BaseMatcher<View>() {
+                    @Override
+                    public boolean matches(Object item) {
+                        return isA(Checkable.class).matches(item);
+                    }
+
+                    @Override
+                    public void describeMismatch(Object item, Description mismatchDescription) {
+                    }
+
+                    @Override
+                    public void describeTo(Description description) {
+                    }
+                };
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                Checkable checkableView = (Checkable) view;
+                if (checkableView.isChecked() != checked) {
+                    checkableView.setChecked(checked);
+                } else if (checkableView.isChecked() == checked) {
+                    checkableView.setChecked(checked);
+                }
+            }
+
+        };
+    }
 }
+
