880657851d41298362cee7cc95a4e2860a1479ce
sanyud24 <sanyu.daver.civ19@itbhu.ac.in>
2022-07-20 00:27:02 +0530

    added table setting test

---
+++ services_app/src/androidTest/java/org/opendatakit/services/preferences/fragments/GeneralTablesSettingsFragmentTest.java
@@ -0,0 +1,64 @@
+package org.opendatakit.services.preferences.fragments;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.Intent;
+
+import androidx.test.espresso.contrib.RecyclerViewActions;
+
+import junit.framework.AssertionFailedError;
+
+import org.junit.Test;
+import org.opendatakit.BaseUITest;
+import org.opendatakit.consts.IntentConsts;
+import org.opendatakit.properties.PropertiesSingleton;
+import org.opendatakit.services.R;
+import org.opendatakit.services.preferences.activities.AppPropertiesActivity;
+
+public class GeneralTablesSettingsFragmentTest extends BaseUITest<AppPropertiesActivity> {
+
+    @Override
+    protected void setUpPostLaunch() {
+        activityScenario.onActivity(activity -> {
+            PropertiesSingleton props = activity.getProps();
+            assertThat(props).isNotNull();
+        });
+
+        onView(withId(R.id.app_properties_content)).check(matches(isDisplayed()));
+        onView(withId(androidx.preference.R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(R.string.tool_tables_settings)),
+                        click()));
+    }
+
+
+    @Test
+    public void whenTableSpecificSettingIsClicked_useCustomHomeScreen() {
+        onView(withText(R.string.custom_home_screen)).check(matches(isDisplayed()));
+        onView(withId(android.R.id.checkbox)).perform(click(), setChecked(true));
+        try {
+            onView(withId(android.R.id.checkbox)).check(matches(isChecked()));
+
+        } catch (AssertionFailedError e) {
+            onView(withId(android.R.id.checkbox)).check(matches(isNotChecked()));
+
+        }
+    }
+
+    @Override
+    protected Intent getLaunchIntent() {
+        Intent intent = new Intent(getContext(), AppPropertiesActivity.class);
+        intent.putExtra(IntentConsts.INTENT_KEY_APP_NAME, APP_NAME);
+        intent.putExtra(IntentConsts.INTENT_KEY_SETTINGS_IN_ADMIN_MODE, true);
+        return intent;
+    }
+
+}
